1322288d755279bf42ff20b2fe8a6b1fc18a59ca
Skiboutz <skiboutz@gmail.com>
2021-03-27 09:32:12 -0400

    added player button

--- main.js
+++ main.js
@@ -1,84 +1,90 @@
 // A map of playerName to an array of playerPER values
+var playerMap = new Map();
 
 
-// Variables to keep track of constants 
-
+// Variables to keep track of constants
+const maxPlayersOnCourt = 5;
+const numQuarters = 4
 
 
 // Variables to track state throughout the game
-
-
+var currentQuarter = 0;
+var playersOnCourt = 0;
+var quarterInPlay = false;
 
 
 // Variables to track the PER throughout the game
-
-
-
+var quarterPER = 0;
+var quarterAvePER = 0;
+var totalAvePER = 0;
 
 // Function to read in all of the player stats
 function processPlayers(allPlayerStats) {
-    // Split the data by newline into an array.
+     
 
+    
 
     // Remove the header line (first line)
+    allPlayerStatLines.shift();
 
 
-    // Loop through the rows and create a map entry of player name to a list of player PER
+    // Get all individual stat values
+    var stats = statLine.split(',');
+    // If it's just an empty line, skip it
+    if (!stats || stats.length <= 1) continue; // empty line
 
-        // Get all individual stat values
+    // The second column has the player name
+    var playerName = stats[1];
 
+    // Check if player exists in map
+    if (!playerMap.has(playerName)) {
+        // First time we see the player; Add them in!
+        playerMap.set(playerName, []);
+    }
 
-        // If it's just an empty line, skip it
+    // Get per value for player
+    var per = parseFloat(stats[9]);
 
-
-        // The second column has the player name
-
-
-        // Check if player exists in map
-
-            // First time we see the player; Add them in!
-
-
-
-        // Get per value for player
-
-
-        // Add per value to player's array (the next quarter)
+    // Add per value to player's array (the next quarter)
+    playerMap.get(playerName).push(per);
 
 
     // Add the players to the bench.
+    displayPlayerBench();
 
 }
 
 // Function to add the players to the bench to start the game
 function displayPlayerBench() {
     // Get the bench div in which the players will be shown.
-
+    var bench = document.getElementById("playersOnBench");
 
     // For each player, create a button 
-
+    for (let playerName of playerMap.keys) {
         // Create a button for each player
-
+        var newPlayer = document.createElement('button');
 
         // Set the ID to the name of the player so we can get it later
-
+        newPlayer.id = playerName;
 
         // Identify the style class, which will set the color scheme
-
+        newPlayer.className = 'playerButton';
 
         // When the button is clicked, call the movePlayer function
-
+        newPlayer.onclick = movePlayer;
 
         // Add the players image to the button
-
+        var playerImage = document.createElement('img');
 
         // Set the source (or location) of the image
-
+        playerImage.src = 'images/'+playerName+'.png';
 
         // Add the image to the button
-
+        newPlayer.appendChild(playerImage);
 
         // Add the button to the bench
+        bench.appendChild(newPlayer);
+    }
 
 
 
@@ -295,6 +301,7 @@ function startNextQuarter() {
     
 
     // Define the interval period for the quarter; in this case, it's 12 seconds.
+    var playerMap = new Map();
 
 
     // Set the quarterInPlay variable to true so that the coach
